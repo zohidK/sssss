e86947452180960ebcf5e8519997e180872ea2e2
zohidK <komilov.github@gmail.com>
2024-11-10 22:31:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
